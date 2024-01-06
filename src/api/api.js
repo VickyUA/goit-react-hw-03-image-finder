079fa303bd1c650515266e4c782e793ff100ce54
VickyUA <vickyy379@gmail.com>
2024-01-06 15:10:55 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const fetchPictures = async (search, page) => {
+export const fetchPictures = async (search, page) => {
   const response = await axios.get(
     `https://pixabay.com/api/?q=${search}&page=${page}&key=41535540-3e2fcae5f9a93b6d79476b27b&image_type=photo&orientation=horizontal&per_page=12`
   );
@@ -8,4 +8,4 @@ const fetchPictures = async (search, page) => {
   return response.data;
 };
 
-export default { fetchPictures };
+export default fetchPictures;
